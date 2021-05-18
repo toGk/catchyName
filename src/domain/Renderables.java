e0ba3e3c93b659687c115006c199d390ec70d4ef
Tsechelidis Michail <tsechelidis.michail@gmail.com>
2021-05-18 14:50:14 +0300

    Renderables implements Serializable

--- src/domain/Renderables.java
+++ src/domain/Renderables.java
@@ -1,11 +1,17 @@
 package domain;
 
+import java.io.Serializable;
+
 import com.game.catchyname.graphics.Screen;
 import com.game.catchyname.graphics.Sprite;
 
 import utilities.Coordinates;
 
-public abstract class Renderables {
+public abstract class Renderables implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	protected Sprite sprite;
 	protected Coordinates spawn;
 	
